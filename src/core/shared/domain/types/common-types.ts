3b7a01a32ba9b47204c18324ae51af7ba18dd7ad
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2025-02-19 20:48:11 -0300

    feat: add WrapperType to address ESM modules circular dependency issue

--- src/core/shared/domain/types/common-types.ts
+++ src/core/shared/domain/types/common-types.ts
@@ -3,3 +3,9 @@ export type Nullable<T> = T | null;
 export type Optional<T> = T | undefined;
 
 export type Replace<T, R> = Omit<T, keyof R> & R;
+
+/**
+ * Wrapper type used to circumvent ESM modules circular dependency issue
+ * caused by reflection metadata saving the type of the property.
+ */
+export type WrapperType<T> = T; // WrapperType === Relation
